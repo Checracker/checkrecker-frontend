c9ed71647ac72112be2512dc6688dd127ebed7a1
bananana0118 <bananana0118@gmail.com>
2024-03-08 22:35:05 +0900

    feat : login api 연결

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/v2/:path*/",
+  //       destination: `https://${process.env.NEXT_PUBLIC_HOST}/:path*/`
+  //     },
+  //   ];
+  // },
+  trailingSlash: true,
   reactStrictMode: false,
   webpack: (config) => {
     config.module.rules.push({
--- src/app/utils/axios.ts
+++ src/app/utils/axios.ts
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const client = axios.create({
-    baseURL:process.env.HOST,
-    timeout: 3000, 
-    headers: {'X-Requested-With': 'XMLHttpRequest'},
+    baseURL: `http://${process.env.NEXT_PUBLIC_HOST}`,
+    timeout: 3000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
