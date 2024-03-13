4999d801d907cf19abde1ca51f32c9f614778c17
yjkang <bananana0118@gmail.com>
2024-03-13 18:37:18 +0900

    feat : axios client 파일 변경경

--- src/app/utils/axios.ts
+++ src/app/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const client = axios.create({
-    baseURL: `http://${process.env.NEXT_PUBLIC_HOST}`,
+    baseURL: `${process.env.NEXT_PUBLIC_HOST}`,
     timeout: 3000,
     headers: {
         'Content-Type': 'application/json',
